cafe22cfbc8a752f69a7b6c23a61d2cc2b134859
Matteo Cargnelutti <matteo.cargnelutti@gmail.com>
2017-10-27 03:39:56 +0200

    Progress : unit tests

---
+++ tests/test_controllers.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python3
+# coding: utf-8
+"""
+Jean-Pierre [Prototype]
+A Raspberry Pi robot helping people to build groceries list.
+Matteo Cargnelutti - github.com/matteocargnelutti
+
+tests/test_controllers.py - Units tests for the controllers package
+"""
