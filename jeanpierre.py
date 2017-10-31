b87f66a470233c82e9e614e7cdf1504f1d15dfc2
Matteo Cargnelutti <matteo.cargnelutti@gmail.com>
2017-10-31 16:20:17 +0100

    Progress : version handling

--- jeanpierre.py
+++ jeanpierre.py
@@ -29,18 +29,28 @@ def main():
     Routing from the console line arguments to the appropriate controller.
     Options :
     --do config/scanner/web
+    Examples :
+    - ./jeanpierre.py --do scanner
+    - ./jeanpierre.py --do config -l fr
+    - ./jeanpierre.py -v
     """
     # Get arguments
     arguments = argparse.ArgumentParser()
     arguments.add_argument("-d", "--do",
                            help="Jean-Pierre's process to launch: config/scanner/web")
     arguments.add_argument("-v", "--version",
-                           help="Jean-Pierre's version.")
+                           help="Jean-Pierre's version.", 
+                           action='store_true')
     arguments.add_argument("-l", "--lang",
                            help="Language for the web app and config assistant. Default: en")
     arguments = arguments.parse_args()
 
-    # If no arguments
+    # Version
+    if arguments.version:
+        print(JEANPIERRE_VERSION)
+        exit()
+
+    # If no "do" argument
     if not arguments or not hasattr(arguments, 'do') or not arguments.do:
         message = "Jean-Pierre understands:\n"
         message += "--do config : launch the configuration assistant"
@@ -49,7 +59,7 @@ def main():
         print(message)
         return
 
-    # If invalid arguments
+    # If invalid "do" argument
     if arguments.do not in ['config', 'scanner', 'web']:
         message = 'Invalid option --do "{}"'.format(arguments.do)
         print(message)
@@ -61,18 +71,13 @@ def main():
         print(message)
         return
 
-    # Version
-    if arguments.version:
-        print(JEANPIERRE_VERSION)
-        exit()
-
-    # Config
+    # Launch controller : Config
     if arguments.do == 'config':
         controllers.Config.execute(str(arguments.lang))
-    # Scanner
+    # Launch controller : Scanner
     elif arguments.do == 'scanner':
         controllers.Scanner.execute()
-    # Web debug
+    # Launch controller : Web debug
     elif arguments.do == 'web':
         webapp.run(debug=True, host='0.0.0.0')
         print("Flask launched in DEBUG mode.")
