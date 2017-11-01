20e49355808ffc021082907959f3e6da2e0254f1
Matteo Cargnelutti <matteo.cargnelutti@gmail.com>
2017-11-01 20:14:40 +0100

    Progress : Web > Tests

--- tests/test_controllers.py
+++ tests/test_controllers.py
@@ -260,7 +260,8 @@ class TestWeb:
 
             expected_data = models.Groceries().get_list()
             given_data = str(response.data, encoding='utf-8')
-            given_data = json.loads(given_data).keys()
+            given_data = json.loads(given_data)
+            given_data = given_data['items'].keys()
 
             assert response.status_code == 200
             assert set(expected_data) == set(given_data)
