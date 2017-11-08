b9e7e1f0f8a823e3455dfabeed4bafd31595f9bb
Matteo Cargnelutti <matteo.cargnelutti@gmail.com>
2017-11-08 01:44:46 +0100

    Progress : Install Bash

--- install.sh
+++ install.sh
@@ -5,7 +5,6 @@
 # Matteo Cargnelutti - github.com/matteocargnelutti
 #
 # install.sh - Setup Bash
-# [!] This script must be executed with root privileges.
 #
 
 #-----------------------------------------------------------------------------
@@ -17,15 +16,15 @@ SUPERVISOR_DIR="/etc/supervisor/conf.d/"
 #-----------------------------------------------------------------------------
 # Execution rights
 #-----------------------------------------------------------------------------
-chmod a+x jeanpierre.py;
-chmod a+x daemon-gunicorn.sh;
-chmod a+x daemon-scanner.sh;
-chmod a+x sass.sh;
+sudo chmod a+x jeanpierre.py;
+sudo chmod a+x scanner.sh;
+sudo chmod a+x web.sh;
+sudo chmod a+x sass.sh;
 
 #-----------------------------------------------------------------------------
 # Dystem dependencies
 #-----------------------------------------------------------------------------
-apt-get install git python3-dev python3-pip python3-picamera virtualenv libzbar0 supervisor libjpeg8-dev;
+sudo apt-get install git python3-dev python3-pip python3-picamera virtualenv libzbar0 supervisor libjpeg8-dev;
 
 #-----------------------------------------------------------------------------
 # Virtual env
@@ -45,25 +44,25 @@ pip install pyzbar[scripts];
 #-----------------------------------------------------------------------------
 # For scanner
 $TO_WRITE = $SUPERVISOR_DIR"jeanpierre-scanner.conf"
-touch $TO_WRITE;
-echo "[program:purbeurre-gunicorn]" >> $TO_WRITE;
-echo "command = ."$JEANPIERRE_DIR"scanner.sh" >> $TO_WRITE;
-echo "user = $USER" >> $TO_WRITE;
-echo "autostart = true" >> $TO_WRITE;
-echo "autorestart = true" >> $TO_WRITE;
+sudo touch $TO_WRITE;
+sudo echo "[program:purbeurre-gunicorn]" >> $TO_WRITE;
+sudo echo "command = ."$JEANPIERRE_DIR"scanner.sh" >> $TO_WRITE;
+sudo echo "user = $USER" >> $TO_WRITE;
+sudo echo "autostart = true" >> $TO_WRITE;
+sudo echo "autorestart = true" >> $TO_WRITE;
 
 # For Gunicorn
 $TO_WRITE = $SUPERVISOR_DIR"jeanpierre-gunicorn.conf"
-touch $TO_WRITE;
-echo "[program:purbeurre-gunicorn]" >> $TO_WRITE;
-echo "command = ."$JEANPIERRE_DIR"web.sh" >> $TO_WRITE;
-echo "user = $USER" >> $TO_WRITE;
-echo "autostart = true" >> $TO_WRITE;
-echo "autorestart = true" >> $TO_WRITE;
+sudo touch $TO_WRITE;
+sudo echo "[program:purbeurre-gunicorn]" >> $TO_WRITE;
+sudo echo "command = ."$JEANPIERRE_DIR"web.sh" >> $TO_WRITE;
+sudo echo "user = $USER" >> $TO_WRITE;
+sudo echo "autostart = true" >> $TO_WRITE;
+sudo echo "autorestart = true" >> $TO_WRITE;
 
 # Init
-supervisorctl reread;
-supervisorctl update;
+sudo supervisorctl reread;
+sudo supervisorctl update;
 
 #-----------------------------------------------------------------------------
 # KTHXBYE
